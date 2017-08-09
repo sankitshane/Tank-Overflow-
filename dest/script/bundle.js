e4e197944488b6602ebeaf2f3458dc1adf89ecd8
sankit acharya <sankit.acharya@gmail.com>
2017-08-10 02:21:35 +0530

    test: Working with React Component to render.
    Testing out ES6 and react Component to test out render of element.

--- dest/script/bundle.js
+++ dest/script/bundle.js
@@ -20806,6 +20806,8 @@ module.exports = require('./lib/React');
 },{"./lib/React":158}],183:[function(require,module,exports){
 'use strict';
 
+var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
+
 var _react = require('react');
 
 var _react2 = _interopRequireDefault(_react);
@@ -20816,10 +20818,42 @@ var _reactDom2 = _interopRequireDefault(_reactDom);
 
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
-_reactDom2.default.render(_react2.default.createElement(
-  'h1',
-  null,
-  'hello'
-), document.getElementById('root'));
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+
+function _possibleConstructorReturn(self, call) { if (!self) { throw new ReferenceError("this hasn't been initialised - super() hasn't been called"); } return call && (typeof call === "object" || typeof call === "function") ? call : self; }
+
+function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function, not " + typeof superClass); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, enumerable: false, writable: true, configurable: true } }); if (superClass) Object.setPrototypeOf ? Object.setPrototypeOf(subClass, superClass) : subClass.__proto__ = superClass; }
+
+var Welcome = function (_React$Component) {
+  _inherits(Welcome, _React$Component);
+
+  function Welcome() {
+    _classCallCheck(this, Welcome);
+
+    var _this = _possibleConstructorReturn(this, (Welcome.__proto__ || Object.getPrototypeOf(Welcome)).call(this));
+
+    _this.state = { title: 'Tank OverFlow' };
+    return _this;
+  }
+
+  _createClass(Welcome, [{
+    key: 'render',
+    value: function render() {
+      return _react2.default.createElement(
+        'div',
+        null,
+        _react2.default.createElement(
+          'h1',
+          null,
+          this.state.title
+        )
+      );
+    }
+  }]);
+
+  return Welcome;
+}(_react2.default.Component);
+
+_reactDom2.default.render(_react2.default.createElement(Welcome, null), document.getElementById('root'));
 
 },{"react":182,"react-dom":29}]},{},[183]);
--- src/script/new.js
+++ src/script/new.js
@@ -1,7 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+class Welcome extends React.Component {
+  constructor() {
+    super();
+    this.state = {title :'Tank OverFlow'}
+    }
+  render() {
+    return(
+      <div>
+        <h1>{this.state.title}</h1>
+      </div>
+    );
+  }
+}
+
 ReactDOM.render(
-  <h1>hello</h1>,
+  <Welcome />,
   document.getElementById('root')
 );
